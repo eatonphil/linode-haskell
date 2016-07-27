b5f61270a75b773678fe42a1815c0b8cbcb2fa70
Phil Eaton <peaton@linode.com>
2016-07-26 21:15:35 -0400

    Use authorization header and environment variable for token

--- linode.hs
+++ linode.hs
@@ -1,21 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
 import Data.Aeson
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 (pack)
 import qualified Data.Maybe (Maybe (Just, Nothing) )
-
 import qualified GHC.Generics (Generic)
 import Network.HTTP.Conduit
+import Network.HTTP.Simple
+import qualified System.Environment (getEnv)
+import qualified Text.Printf (printf)
 
 apiRoot = "https://api.alpha.linode.com/v4"
 
-data ListResponse t = ListResponse { total_results :: Int,
-                                     total_pages :: Int,
-                                     page :: Int,
-                                     list :: [t] } deriving (Show, GHC.Generic.Generic)
+getToken :: IO String
+getToken = System.Environment.getEnv "LINODE_TOKEN"
 
 data Datacenter = Datacenter { id :: String,
                                datacenter :: String,
@@ -24,15 +27,28 @@ data Datacenter = Datacenter { id :: String,
 instance Data.Aeson.FromJSON Datacenter
 instance Data.Aeson.ToJSON Datacenter
 
-data Datacenters = ListResponse Datacenter
+data Datacenters = Datacenters { total_pages :: Int,
+                                 total_results :: Int,
+                                 page :: Int,
+                                 datacenters :: [Datacenter] } deriving (Show, GHC.Generics.Generic)
 
 instance Data.Aeson.FromJSON Datacenters
 instance Data.Aeson.ToJSON Datacenters
 
+getData :: Data.Aeson.FromJSON a => String -> IO (Data.Maybe.Maybe a)
+getData endpoint = do
+  token <- getToken
+  let authorizationValue :: S.ByteString =
+        Data.ByteString.Char8.pack
+        $ Text.Printf.printf "token %s" token
+  let request =
+        addRequestHeader "Authorization" authorizationValue
+        $ parseRequest_ (Text.Printf.printf "GET %s%s" apiRoot endpoint)
+  response <- httpJSON request
+  return (getResponseBody response)
+
 getDatacenters :: IO (Data.Maybe.Maybe Datacenters)
-getDatacenters = do
-  rawJson <- simpleHttp (apiRoot ++ "/datacenters")
-  return (decode rawJson :: Data.Maybe.Maybe Datacenters)
+getDatacenters = getData "/datacenters"
 
 data Distribution = Distribution { id :: String,
                                    created :: String,
@@ -45,10 +61,70 @@ data Distribution = Distribution { id :: String,
 instance Data.Aeson.FromJSON Distribution
 instance Data.Aeson.ToJSON Distribution
 
-data Distributions
+data Distributions = Distributions { total_pages :: Int,
+                                     total_results :: Int,
+                                     page :: Int,
+                                     distributions :: [Distributions] } deriving (Show, GHC.Generics.Generic)
+
+instance Data.Aeson.FromJSON Distributions
+instance Data.Aeson.ToJSON Distributions
+
+getDistributions :: IO (Data.Maybe.Maybe Distributions)
+getDistributions = getData "/distributions"
+
+data Linode = Linode { id :: String,
+                       label :: String,
+                       created :: String,
+                       group :: String,
+                       label :: String,
+                       state :: String,
+                       total_transfer :: Int,
+                       updated :: String } deriving (Show, GHC.Generics.Generic)
+
+instance Data.Aeson.FromJSON Linode
+instance Data.Aeson.ToJSON Linode
+
+data DNSZone = DNSZone { id :: String,
+                         dnszone :: String,
+                         soa_email :: String,
+                         description :: String,
+                         refresh_sec :: Int,
+                         retry_sec :: Int,
+                         expire_sec :: Int,
+                         ttl_sec :: Int,
+                         status :: String,
+                         master_ips :: [String],
+                         axfr_ips :: [String],
+                         display_group :: String,
+                         type :: String } deriving (Show, GHC.Generics.Generic)
+
+instance Data.Aeson.FromJSON DNSZone
+instance Data.Aeson.ToJSON DNSZone
+
+data DNSZones = DNSZones { total_pages :: Int,
+                           total_results :: Int,
+                           page :: Int,
+                           dnszones :: [DNSZone] } deriving (Show, GHC.Generics.Generic)
+
+instance Data.Aeson.FromJSON DNSZones
+instance Data.Aeson.ToJSON DNSZones
+
+getDNSZones :: IO (Data.Maybe.Maybe DNSZones)
+getDNSZones = getData "/dnszones"
+
+data Linodes = Linodes { total_pages :: Int,
+                         total_results :: Int,
+                         page :: Int,
+                         linodes :: [Linode] } deriving (Show, GHC.Generics.Generic)
+
+instance Data.Aeson.FromJSON Linodes
+instance Data.Aeson.ToJSON Linodes
+
+getLinodes :: IO (Data.Maybe.Maybe Linodes)
+getLinodes = getData "/linodes"
 
 main = do
-  response <- getDatacenters
+  response <- getLinodes
   case response of
-    (Data.Maybe.Just v) -> print (show v)
-    Data.Maybe.Nothing  -> print "Error"
+    Just v -> print $ show v
+    Nothing -> print "Error"
